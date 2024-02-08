174214ff036f1d2fff0f75020f47e4b2e6f37386
Jason Ozias <jason.g.ozias@gmail.com>
2024-02-08 10:18:30 -0500

    removed lint

--- src/lib.rs
+++ src/lib.rs
@@ -256,7 +256,6 @@
         non_shorthand_field_patterns,
         non_snake_case,
         non_upper_case_globals,
-        nontrivial_structural_match,
         noop_method_call,
         overlapping_range_endpoints,
         path_statements,
